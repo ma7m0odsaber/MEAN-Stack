50a9072c3e00edb7cd2ed17326e3ed91cf056ccf
Mahmoud <rodomahmoud121@gmail.com>
2025-12-28 20:46:48 +0200

    Tasks in the lecture

--- Lecture 1/script.js
+++ Lecture 1/script.js
@@ -11,64 +11,89 @@
 // console.log(x1 * 20);
 // console.log(x3 / x2);
 
-// let btn = document.getElementById('submit');
-// addEventListener(btn, submit());
+let items = ["apple", "juice"];
+function submit() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
 
-// let items = ['Apple', 'Juice'];
+  let sell = document.getElementById("sell");
+  let buy = document.getElementById("buy");
+  let rbtn = document.getElementById("rbtn");
 
-// function submit() {
-//   let username = document.getElementById("username");
-//   let password = document.getElementById("password");
+  let login = document.getElementById("login");
 
-//   let user = username.value;
-//   let pass = password.value;
-//   console.log(user);
-//   console.log(pass);
+  if (username === "ma7m0od_saber") {
+    if (password === "12345678") {
+      alert("Welcome " + username);
+      login.style.display = "none";
+      sell.style.display = "block";
+      rbtn.style.display = "block";
+    } else {
+      alert("Sorry, Password is incorrect!");
+    }
+  } else {
+    alert("Welcome " + username);
+    login.style.display = "none";
+    buy.style.display = "block";
+    rbtn.style.display = "block";
 
-//     let sell = document.getElementById('sell');
-//     let buy = document.getElementById('buy');
+    let z = document.getElementById("itemList");
 
-//     let login = document.getElementById('login');
+    z.innerHTML = "";
 
-//     if (user === "ma7m0od_saber") {
-//         if (pass === "12345678") {
-//             alert("Welcome " + user);
-//             // login.setAttribute(hidden);
-//             // sell.removeAttribute(hidden);
-//         } else {
-//             alert("Sorry, Password is incorrect!");
-//         }
-//     } else {
-//         // buy.removeAttribute(hidden);
-//     }
-// }
+    for (let i = 0; i < items.length; i++) {
+      let t = document.createElement("button");
+      t.onclick = buyingItems;
+      t.id = "item";
+      t.textContent = items[i];
+      z.appendChild(t);
+    }
+  }
+}
 
-// let z = document.getElementById('itemList');
-// function sellingItems() {
-//   let y = document.getElementById('sellItem').value;
-//   items.push(y);
-//   alert("Now, you can sell " + y);
-//   for(let i=0;i<3;i++){
-//       let t = document.createElement('Option', 'value'=items[i]);
-//       z.appendChild(t);
-//   }
-// }
+function sellingItems() {
+  let y = document.getElementById("sellItem").value;
 
-// function buyingItems() {
-//   switch (z) {
-//     case "apple":
-//       alert("apple bought successfully");
-//       break;
-//     case "juice":
-//       alert("juice bought successfully");
-//       break;
-//     case "tomatoes":
-//       alert("tomatoes bought successfully");
-//       break;
-//     default:
-//       alert("You can not buy this item!");
-//   }
-// }
+  items.push(y);
+  alert("Now, you can sell " + y);
+}
+
+function buyingItems() {
+  let item = document.getElementById("item");
+
+  alert("You succesfully bought " + item.textContent);
+
+  //   switch (z.value) {
+  //     case "apple":
+  //       alert("apple bought successfully");
+  //       break;
+  //     case "juice":
+  //       alert("juice bought successfully");
+  //       break;
+  //     default:
+  //       let f = 0;
+  //       for (let i = 0; i < items.length; i++) {
+  //         if (z.value == items[i]) {
+  //           f = 1;
+  //           alert(z.value + " bought successfully");
+  //           break;
+  //         }
+  //       }
+  //       if (f == 0) alert("You can not buy this item!");
+  //   }
+}
+
+function returnbtn() {
+  let sell = document.getElementById("sell");
+  let buy = document.getElementById("buy");
+  let login = document.getElementById("login");
+  let rbtn = document.getElementById("rbtn");
+
+  login.style.display = "block";
+  sell.style.display = "none";
+  buy.style.display = "none";
+  rbtn.style.display = "none";
+}
 
 // btn = document.getElementsByTagName('button');
 // addEventListener(btn, alertUser());
@@ -82,8 +107,8 @@
 // }
 // alert(sum);
 
-let arr = ['apple', 'lemon'];
+// let arr = ['apple', 'lemon'];
 
-for (const i of arr) {
-    alert('I will eat ' + i);
-}
+// for (const i of arr) {
+//     alert('I will eat ' + i);
+// }
